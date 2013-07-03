3c77fceb2b4015e7558a25b7b2a5d93be6e3b1dd
Jakub Hampl <honitom@seznam.cz>
2013-07-03 14:39:47 +0100

    Initial Commit

---
+++ CSS Editor/GMDocument.h
@@ -0,0 +1,13 @@
+//
+//  GMDocument.h
+//  CSS Editor
+//
+//  Created by Jakub Hampl on 03.07.13.
+//  Copyright (c) 2013 Jakub Hampl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GMDocument : NSDocument
+
+@end
---
+++ CSS Editor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CSS Editor
+//
+//  Created by Jakub Hampl on 03.07.13.
+//  Copyright (c) 2013 Jakub Hampl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
